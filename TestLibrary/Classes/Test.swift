053007eb0fbe8b7bd6c07f894d3afc9ac5614cfb
Ilham Hadi Prabawa <iprabawa9@gmail.com>
2018-11-28 18:50:28 +0700

    remove class

---
+++ TestLibrary/Classes/Test.swift
@@ -0,0 +1,15 @@
+//
+//  Test.swift
+//  Pods-TestLibrary_Example
+//
+//  Created by Ilham Hadi Prabawa on 11/28/18.
+//
+
+import Foundation
+
+public class Test: NSObject {
+    
+    public static func show(){
+        print("Test")
+    }
+}
